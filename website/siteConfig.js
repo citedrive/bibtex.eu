acba1d0ac40218558bb0c8e921619d1cb6349fb3
garethbilaney <gareth.bilaney@hhu.de>
2020-10-10 14:11:24 +0200

    ...

---
+++ website/siteConfig.js
@@ -0,0 +1,130 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// See https://docusaurus.io/docs/site-config for all the possible
+// site configuration options.
+
+// List of projects/orgs using your project for the users page.
+
+
+
+const users = [
+  {
+    caption: 'Citedrive',
+    // You will need to prepend the image path with your baseUrl
+    // if it is not '/', like: '/test-site/img/image.jpg'.
+    image: '/img/citedrive_logo.svg',
+    infoLink: 'https://www.citedrive.com',
+    pinned: true,
+  },
+  {
+    caption: 'BehaviorCloud',
+    // You will need to prepend the image path with your baseUrl
+    // if it is not '/', like: '/test-site/img/image.jpg'.
+    image: '/img/citedrive_logo.svg',
+    infoLink: 'https://www.citedrive.com',
+    pinned: true,
+  },
+  {
+    caption: 'Bilaney Consultants GmbH',
+    // You will need to prepend the image path with your baseUrl
+    // if it is not '/', like: '/test-site/img/image.jpg'.
+    image: '/img/citedrive_logo.svg',
+    infoLink: 'https://www.bilaney.com',
+    pinned: true,
+  },
+];
+
+const siteConfig = {
+  title: 'BibTeX.eu', // Title for your website.
+  tagline: 'Documents, guidelines and workshops for BibTeX, BibLatex and bibliography management at LaTeX. By Citedrive.com',
+  url: 'https://bibtex.eu', // Your website URL
+  baseUrl: '/', // Base URL for your project */
+  // For github.io type URLs, you would set the url and baseUrl like:
+  //   url: 'https://facebook.github.io',
+  //   baseUrl: '/test-site/',
+
+  // Used for publishing and more
+  projectName: 'bibtex-citedrive',
+  organizationName: 'Citedrive',
+  // For top-level user or org sites, the organization is still the same.
+  // e.g., for the https://JoelMarcey.github.io site, it would be set like...
+  //   organizationName: 'JoelMarcey'
+
+  // For no header links in the top nav bar -> headerLinks: [],
+  headerLinks: [
+    {doc: 'doc1', label: 'Docs'},
+    {doc: 'doc4', label: 'API'},
+    {page: 'help', label: 'Help'},
+    {page: 'help-with-translations', label: 'Translate'},
+    {blog: true, label: 'Blog'},
+  ],
+
+  // If you have users set above, you add it here:
+  users,
+
+  /* path to images for header/footer */
+  headerIcon: 'img/citedrive_logo.svg',
+  footerIcon: 'img/citedrive_logo.svg',
+  favicon: 'img/citedrive_logo.svg',
+
+  /* Colors for website */
+  colors: {
+    primaryColor: 'fff',
+    secondaryColor: '#333',
+  },
+
+  /* Custom fonts for website */
+  /*
+  fonts: {
+    myFont: [
+      "Times New Roman",
+      "Serif"
+    ],
+    myOtherFont: [
+      "-apple-system",
+      "system-ui"
+    ]
+  },
+  */
+
+  // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
+  copyright: `Copyright © ${new Date().getFullYear()} Citedrive`,
+
+  highlight: {
+    // Highlight.js theme to use for syntax highlighting in code blocks.
+    theme: 'default',
+  },
+
+  // Add custom scripts here that would be placed in <script> tags.
+  scripts: ['https://buttons.github.io/buttons.js'],
+
+  // On page navigation for the current documentation page.
+  onPageNav: 'separate',
+  // No .html extensions for paths.
+  cleanUrl: true,
+
+  // Open Graph and Twitter card images.
+  ogImage: 'img/undraw_online.svg',
+  twitterImage: 'img/undraw_tweetstorm.svg',
+
+  // For sites with a sizable amount of content, set collapsible to true.
+  // Expand/collapse the links and subcategories under categories.
+  // docsSideNavCollapsible: true,
+
+  // Show documentation's last contributor's name.
+  // enableUpdateBy: true,
+
+  // Show documentation's last update time.
+  // enableUpdateTime: true,
+
+  // You may provide arbitrary config keys to be used as needed by your
+  // template. For example, if you need your repo's URL...
+  // repoUrl: 'https://github.com/facebook/test-site',
+};
+
+module.exports = siteConfig;
