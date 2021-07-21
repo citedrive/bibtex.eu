4bb854f345f05aa7f71c00038f31d0fb28edbe08
Gareth Bilaney <gareth.bilaney@hhu.de>
2021-07-21 10:30:37 +0200

    ...

--- src/pages/overleaf.js
+++ src/pages/overleaf.js
@@ -5,20 +5,22 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
+import OverleafCiteDrive from './overleaf_citedrive.svg';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">CiteDrive + Overleaf</h1>
-        <p className="hero__subtitle">Complete your research pipeline with the powerful one-two pair app.</p>
-        <p className=".hero__sub__subtitle">Connect your both APPs, and you will get all updates in your BibTeX file without having to do anything manually in Overleaf - with a few mouse clicks.</p>
-        <p className=".hero__sub__subtitle">Focus on your work at Overleaf and leave the tedious formatting of bibliographies and citations to us. </p>
+        <OverleafCiteDrive />
+
+        <p style ={{    color: "#000"}} className={styles.subsubtitle}>Complete your research infrastructure</p>
+        <p className={styles.subsubsubtitle}>Complete your research pipeline with the powerful one-two pair app. Connect your both APPs, and you will get all updates in your BibTeX file without having to do anything manually in Overleaf - with a few mouse clicks. Focus on your work at Overleaf and leave the tedious formatting of bibliographies and citations to us. </p>
+
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="https://app.citedrive.com/">
             Getting started
           </Link>
         </div>
@@ -33,7 +35,7 @@ function HomepageExplainer() {
   const {siteConfig} = useDocusaurusContext();
   return (
 <div className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
+      <div className="container" style={{background: "#000"}}>
         <p>Overleaf enables teams of students and researchers to write papers simultaneously, from anywhere, in an easy-to-use, collaborative online LaTeX editor. Overleaf supports a variety of packages, including BibTeX, where you can manage your references.
 
 When you connect Overleaf to CiteDrive, you can automatically synchronize your project and BibTeX-database with Overleaf.
@@ -49,12 +51,14 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`Overleaf + CiteDrive`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
       <HomepageExplainer />
+      {/*
         <HomepageFeatures />
+      */}
       </main>
     </Layout>
   );
