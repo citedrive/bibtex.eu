f30ab6644de85b6787bb04cd8c9af34d544254e5
garethbilaney <gareth.bilaney@hhu.de>
2023-09-30 11:18:31 +0200

    Update CiteDrive.js

--- src/components/CiteDrive.js
+++ src/components/CiteDrive.js
@@ -11,7 +11,7 @@ const CiteDrive = () => {
         loading="lazy"
         width="100%"
         height="615"
-        src="https://www.youtube.com/embed/bHD94qM0vyg?si=yfHhqeyn161JYoSt&autoplay=1"
+        src="https://www.youtube.com/embed/bHD94qM0vyg?si=yfHhqeyn161JYoSt"
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
