055fd8e6f4dbb6b55ba02bfc58a77a06c55d8215
garethbilaney <gareth.bilaney@hhu.de>
2024-03-05 12:10:58 +0100

    extend schema and find all tags

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,14 @@
-import { defineCollection } from 'astro:content';
+import { z, defineCollection } from 'astro:content';
 import { docsSchema, i18nSchema } from '@astrojs/starlight/schema';
 
+
 export const collections = {
-	docs: defineCollection({ schema: docsSchema() }),
+	docs: defineCollection({ schema: docsSchema({
+		extend: z.object({
+			tags: z.array(z.string()).optional(),
+		})
+	}) }),
 	i18n: defineCollection({ type: 'data', schema: i18nSchema() }),
 };
+
+
