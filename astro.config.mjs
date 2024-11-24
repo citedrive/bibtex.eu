76da3659bfd665f19a4625ad4ce52b2a01b802b0
garethbilaney <gareth.bilaney@hhu.de>
2024-11-24 22:05:08 +0100

    ...

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
 const site = 'https://bibtex.eu/';
 
 
@@ -8,16 +9,14 @@ const site = 'https://bibtex.eu/';
 export default defineConfig({
   site,
   integrations: [starlight({
-    head: [
-      {
-        tag: 'script',
-        attrs: {
-          async: true,
-          src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9078114372269384',
-          'crossorigin': 'anonymous',
-        },
-      },
-    ],
+    head: [{
+      tag: 'script',
+      attrs: {
+        async: true,
+        src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9078114372269384',
+        'crossorigin': 'anonymous'
+      }
+    }],
     title: 'BibTeX',
     logo: {
       light: './src/assets/images/logo.svg',
@@ -94,147 +93,139 @@ export default defineConfig({
         lang: 'ru-RU'
       }
     },
-    sidebar: [
-      {
-        label: 'Basics',
+    sidebar: [{
+      label: 'Basics',
+      translations: {
+        "de-DE": "Grundlagen",
+        "cs-CZ": "Základy",
+        "es-ES": "Conceptos básicos",
+        "pt-PT": "Noções básicas",
+        "fr-FR": "Fondamentaux",
+        "sv-SE": "Grundläggande",
+        "tr-TR": "Temel",
+        "zh-CN": "基础",
+        "da-DK": "Grundlæggende",
+        "it-IT": "Principi",
+        "ja-JP": "基本",
+        "ko-KR": "기초",
+        "no-NO": "Grunnleggende",
+        "ru-RU": "Основы"
+      },
+      items: [{
+        label: 'Using LaTeX (Quick start)',
+        link: '/',
         translations: {
-          "de-DE": "Grundlagen",
-          "cs-CZ": "Základy",
-          "es-ES": "Conceptos básicos",
-          "pt-PT": "Noções básicas",
-          "fr-FR": "Fondamentaux",
-          "sv-SE": "Grundläggande",
-          "tr-TR": "Temel",
-          "zh-CN": "基础",
-          "da-DK": "Grundlæggende",
-          "it-IT": "Principi",
-          "ja-JP": "基本",
-          "ko-KR": "기초",
-          "no-NO": "Grunnleggende",
-          "ru-RU": "Основы"
-        },
-        items: [
-          {
-            label: 'Using LaTeX (Quick start)',
-            link: '/',
-            translations: {
-              "de-DE": "Verwendung von LaTeX (Schnellstart)",
-              "cs-CZ": "Použití LaTeXu (Rychlý start)",
-              "es-ES": "Uso de LaTeX (Inicio rápido)",
-              "pt-PT": "Usando LaTeX (Início rápido)",
-              "fr-FR": "Utilisation de LaTeX (Démarrage rapide)",
-              "sv-SE": "Använda LaTeX (Snabbstart)",
-              "tr-TR": "LaTeX Kullanımı (Hızlı başlangıç)",
-              "zh-CN": "使用 LaTeX（快速入门）",
-              "da-DK": "Brug af LaTeX (Hurtig start)",
-              "it-IT": "Utilizzo di LaTeX (Avvio rapido)",
-              "ja-JP": "LaTeX の使用（クイックスタート）",
-              "ko-KR": "LaTeX 사용하기 (빠른 시작)",
-              "no-NO": "Bruk av LaTeX (Hurtigstart)",
-              "ru-RU": "Использование LaTeX (Быстрый старт)"
-            }
-          },
-          {
-            label: 'Natbib (In-depth Tutorial)',
-            link: '/natbib/',
-            translations: {
-              "de-DE": "Natbib (Ausführliches Tutorial)",
-              "cs-CZ": "Natbib (Podrobný průvodce)",
-              "es-ES": "Natbib (Tutorial detallado)",
-              "pt-PT": "Natbib (Tutorial detalhado)",
-              "fr-FR": "Natbib (Tutoriel approfondi)",
-              "sv-SE": "Natbib (Djupgående handledning)",
-              "tr-TR": "Natbib (Detaylı Kılavuz)",
-              "zh-CN": "Natbib（深入教程）",
-              "da-DK": "Natbib (Dybdegående vejledning)",
-              "it-IT": "Natbib (Tutorial approfondito)",
-              "ja-JP": "Natbib（詳細なチュートリアル）",
-              "ko-KR": "Natbib 사용법 (심층 튜토리얼)",
-              "no-NO": "Natbib (Dypgående veiledning)",
-              "ru-RU": "Natbib (Подробное руководство)"
-            }
-          },
-          {
-            label: 'Using BibLaTeX (Quick start)',
-            link: '/biblatex/',
-            translations: {
-              "de-DE": "Verwendung von BibLaTeX (Schnellstart)",
-              "cs-CZ": "Použití BibLaTeXu (Rychlý start)",
-              "es-ES": "Uso de BibLaTeX (Inicio rápido)",
-              "pt-PT": "Usando BibLaTeX (Início rápido)",
-              "fr-FR": "Utilisation de BibLaTeX (Démarrage rapide)",
-              "sv-SE": "Använda BibLaTeX (Snabbstart)",
-              "tr-TR": "BibLaTeX Kullanımı (Hızlı başlangıç)",
-              "zh-CN": "使用 BibLaTeX（快速入门）",
-              "da-DK": "Brug af BibLaTeX (Hurtig start)",
-              "it-IT": "Utilizzo di BibLaTeX (Avvio rapido)",
-              "ja-JP": "BibLaTeX の使用（クイックスタート）",
-              "ko-KR": "BibLaTeX 사용하기 (빠른 시작)",
-              "no-NO": "Bruk av BibLaTeX (Hurtigstart)",
-              "ru-RU": "Использование BibLaTeX (Быстрый старт)"
-            }
-          },
-          {
-            label: 'Using Quarto (RStudio/Posit) (Quick start)',
-            link: '/quarto/',
-            translations: {
-              "de-DE": "Verwendung von Quarto (RStudio/Posit) (Schnellstart)",
-              "cs-CZ": "Použití Quarto (RStudio/Posit) (Rychlý start)",
-              "es-ES": "Uso de Quarto (RStudio/Posit) (Inicio rápido)",
-              "pt-PT": "Usando Quarto (RStudio/Posit) (Início rápido)",
-              "fr-FR": "Utilisation de Quarto (RStudio/Posit) (Démarrage rapide)",
-              "sv-SE": "Använda Quarto (RStudio/Posit) (Snabbstart)",
-              "tr-TR": "Quarto Kullanımı (RStudio/Posit) (Hızlı başlangıç)",
-              "zh-CN": "使用 Quarto (RStudio/Posit)（快速入门）",
-              "da-DK": "Brug af Quarto (RStudio/Posit) (Hurtig start)",
-              "it-IT": "Utilizzo di Quarto (RStudio/Posit) (Avvio rapido)",
-              "ja-JP": "Quarto の使用（RStudio/Posit）（クイックスタート）",
-              "ko-KR": "Quarto 사용하기 (RStudio/Posit) (빠른 시작)",
-              "no-NO": "Bruk av Quarto (RStudio/Posit) (Hurtigstart)",
-              "ru-RU": "Использование Quarto (RStudio/Posit) (Быстрый старт)"
-            }
-          },
-          {
-            label: 'Using R Markdown (Quick start)',
-            link: '/rmarkdown/',
-            translations: {
-              "de-DE": "Verwendung von R Markdown (Schnellstart)",
-              "cs-CZ": "Použití R Markdown (Rychlý start)",
-              "es-ES": "Uso de R Markdown (Inicio rápido)",
-              "pt-PT": "Usando R Markdown (Início rápido)",
-              "fr-FR": "Utilisation de R Markdown (Démarrage rapide)",
-              "sv-SE": "Använda R Markdown (Snabbstart)",
-              "tr-TR": "R Markdown Kullanımı (Hızlı başlangıç)",
-              "zh-CN": "使用 R Markdown（快速入门）",
-              "da-DK": "Brug af R Markdown (Hurtig start)",
-              "it-IT": "Utilizzo di R Markdown (Avvio rapido)",
-              "ja-JP": "R Markdown の使用（クイックスタート）",
-              "ko-KR": "R Markdown 사용하기 (빠른 시작)",
-              "no-NO": "Bruk av R Markdown (Hurtigstart)",
-              "ru-RU": "Использование R Markdown (Быстрый старт)"
-            }
-          },
-          {
-            label: 'Using Overleaf (Quick start)',
-            link: '/overleaf/',
-            translations: {
-              "de-DE": "Verwendung von Overleaf (Schnellstart)",
-              "cs-CZ": "Použití Overleaf (Rychlý start)",
-              "es-ES": "Uso de Overleaf (Inicio rápido)",
-              "pt-PT": "Usando Overleaf (Início rápido)",
-              "fr-FR": "Utilisation d'Overleaf (Démarrage rapide)",
-              "sv-SE": "Använda Overleaf (Snabbstart)",
-              "tr-TR": "Overleaf Kullanımı (Hızlı başlangıç)",
-              "zh-CN": "使用 Overleaf（快速入门）",
-              "da-DK": "Brug af Overleaf (Hurtig start)",
-              "it-IT": "Utilizzo di Overleaf (Avvio rapido)",
-              "ja-JP": "Overleaf の使用（クイックスタート）",
-              "ko-KR": "Overleaf 사용하기 (빠른 시작)",
-              "no-NO": "Bruk av Overleaf (Hurtigstart)",
-              "ru-RU": "Использование Overleaf (Быстрый старт)"
-            }
-          }
-        ]
+          "de-DE": "Verwendung von LaTeX (Schnellstart)",
+          "cs-CZ": "Použití LaTeXu (Rychlý start)",
+          "es-ES": "Uso de LaTeX (Inicio rápido)",
+          "pt-PT": "Usando LaTeX (Início rápido)",
+          "fr-FR": "Utilisation de LaTeX (Démarrage rapide)",
+          "sv-SE": "Använda LaTeX (Snabbstart)",
+          "tr-TR": "LaTeX Kullanımı (Hızlı başlangıç)",
+          "zh-CN": "使用 LaTeX（快速入门）",
+          "da-DK": "Brug af LaTeX (Hurtig start)",
+          "it-IT": "Utilizzo di LaTeX (Avvio rapido)",
+          "ja-JP": "LaTeX の使用（クイックスタート）",
+          "ko-KR": "LaTeX 사용하기 (빠른 시작)",
+          "no-NO": "Bruk av LaTeX (Hurtigstart)",
+          "ru-RU": "Использование LaTeX (Быстрый старт)"
+        }
+      }, {
+        label: 'Natbib (In-depth Tutorial)',
+        link: '/natbib/',
+        translations: {
+          "de-DE": "Natbib (Ausführliches Tutorial)",
+          "cs-CZ": "Natbib (Podrobný průvodce)",
+          "es-ES": "Natbib (Tutorial detallado)",
+          "pt-PT": "Natbib (Tutorial detalhado)",
+          "fr-FR": "Natbib (Tutoriel approfondi)",
+          "sv-SE": "Natbib (Djupgående handledning)",
+          "tr-TR": "Natbib (Detaylı Kılavuz)",
+          "zh-CN": "Natbib（深入教程）",
+          "da-DK": "Natbib (Dybdegående vejledning)",
+          "it-IT": "Natbib (Tutorial approfondito)",
+          "ja-JP": "Natbib（詳細なチュートリアル）",
+          "ko-KR": "Natbib 사용법 (심층 튜토리얼)",
+          "no-NO": "Natbib (Dypgående veiledning)",
+          "ru-RU": "Natbib (Подробное руководство)"
+        }
+      }, {
+        label: 'Using BibLaTeX (Quick start)',
+        link: '/biblatex/',
+        translations: {
+          "de-DE": "Verwendung von BibLaTeX (Schnellstart)",
+          "cs-CZ": "Použití BibLaTeXu (Rychlý start)",
+          "es-ES": "Uso de BibLaTeX (Inicio rápido)",
+          "pt-PT": "Usando BibLaTeX (Início rápido)",
+          "fr-FR": "Utilisation de BibLaTeX (Démarrage rapide)",
+          "sv-SE": "Använda BibLaTeX (Snabbstart)",
+          "tr-TR": "BibLaTeX Kullanımı (Hızlı başlangıç)",
+          "zh-CN": "使用 BibLaTeX（快速入门）",
+          "da-DK": "Brug af BibLaTeX (Hurtig start)",
+          "it-IT": "Utilizzo di BibLaTeX (Avvio rapido)",
+          "ja-JP": "BibLaTeX の使用（クイックスタート）",
+          "ko-KR": "BibLaTeX 사용하기 (빠른 시작)",
+          "no-NO": "Bruk av BibLaTeX (Hurtigstart)",
+          "ru-RU": "Использование BibLaTeX (Быстрый старт)"
+        }
+      }, {
+        label: 'Using Quarto (RStudio/Posit) (Quick start)',
+        link: '/quarto/',
+        translations: {
+          "de-DE": "Verwendung von Quarto (RStudio/Posit) (Schnellstart)",
+          "cs-CZ": "Použití Quarto (RStudio/Posit) (Rychlý start)",
+          "es-ES": "Uso de Quarto (RStudio/Posit) (Inicio rápido)",
+          "pt-PT": "Usando Quarto (RStudio/Posit) (Início rápido)",
+          "fr-FR": "Utilisation de Quarto (RStudio/Posit) (Démarrage rapide)",
+          "sv-SE": "Använda Quarto (RStudio/Posit) (Snabbstart)",
+          "tr-TR": "Quarto Kullanımı (RStudio/Posit) (Hızlı başlangıç)",
+          "zh-CN": "使用 Quarto (RStudio/Posit)（快速入门）",
+          "da-DK": "Brug af Quarto (RStudio/Posit) (Hurtig start)",
+          "it-IT": "Utilizzo di Quarto (RStudio/Posit) (Avvio rapido)",
+          "ja-JP": "Quarto の使用（RStudio/Posit）（クイックスタート）",
+          "ko-KR": "Quarto 사용하기 (RStudio/Posit) (빠른 시작)",
+          "no-NO": "Bruk av Quarto (RStudio/Posit) (Hurtigstart)",
+          "ru-RU": "Использование Quarto (RStudio/Posit) (Быстрый старт)"
+        }
+      }, {
+        label: 'Using R Markdown (Quick start)',
+        link: '/rmarkdown/',
+        translations: {
+          "de-DE": "Verwendung von R Markdown (Schnellstart)",
+          "cs-CZ": "Použití R Markdown (Rychlý start)",
+          "es-ES": "Uso de R Markdown (Inicio rápido)",
+          "pt-PT": "Usando R Markdown (Início rápido)",
+          "fr-FR": "Utilisation de R Markdown (Démarrage rapide)",
+          "sv-SE": "Använda R Markdown (Snabbstart)",
+          "tr-TR": "R Markdown Kullanımı (Hızlı başlangıç)",
+          "zh-CN": "使用 R Markdown（快速入门）",
+          "da-DK": "Brug af R Markdown (Hurtig start)",
+          "it-IT": "Utilizzo di R Markdown (Avvio rapido)",
+          "ja-JP": "R Markdown の使用（クイックスタート）",
+          "ko-KR": "R Markdown 사용하기 (빠른 시작)",
+          "no-NO": "Bruk av R Markdown (Hurtigstart)",
+          "ru-RU": "Использование R Markdown (Быстрый старт)"
+        }
+      }, {
+        label: 'Using Overleaf (Quick start)',
+        link: '/overleaf/',
+        translations: {
+          "de-DE": "Verwendung von Overleaf (Schnellstart)",
+          "cs-CZ": "Použití Overleaf (Rychlý start)",
+          "es-ES": "Uso de Overleaf (Inicio rápido)",
+          "pt-PT": "Usando Overleaf (Início rápido)",
+          "fr-FR": "Utilisation d'Overleaf (Démarrage rapide)",
+          "sv-SE": "Använda Overleaf (Snabbstart)",
+          "tr-TR": "Overleaf Kullanımı (Hızlı başlangıç)",
+          "zh-CN": "使用 Overleaf（快速入门）",
+          "da-DK": "Brug af Overleaf (Hurtig start)",
+          "it-IT": "Utilizzo di Overleaf (Avvio rapido)",
+          "ja-JP": "Overleaf の使用（クイックスタート）",
+          "ko-KR": "Overleaf 사용하기 (빠른 시작)",
+          "no-NO": "Bruk av Overleaf (Hurtigstart)",
+          "ru-RU": "Использование Overleaf (Быстрый старт)"
+        }
+      }]
     }, {
       collapsed: true,
       label: 'Reference Types',
@@ -254,7 +245,6 @@ export default defineConfig({
         "no-NO": "Referansetyper",
         "ru-RU": "Типы ссылок"
       },
-      
       autogenerate: {
         directory: 'types'
       }
@@ -283,22 +273,22 @@ export default defineConfig({
     }, {
       collapsed: true,
       label: 'BibTeX FAQ',
-  translations: {
-    "de-DE": "BibTeX FAQ",
-    "cs-CZ": "BibTeX FAQ",
-    "es-ES": "Preguntas frecuentes sobre BibTeX",
-    "pt-PT": "FAQ do BibTeX",
-    "fr-FR": "FAQ BibTeX",
-    "sv-SE": "BibTeX FAQ",
-    "tr-TR": "BibTeX SSS",
-    "zh-CN": "BibTeX 常见问题",
-    "da-DK": "BibTeX FAQ",
-    "it-IT": "FAQ su BibTeX",
-    "ja-JP": "BibTeX FAQ",
-    "ko-KR": "BibTeX FAQ",
-    "no-NO": "BibTeX FAQ",
-    "ru-RU": "BibTeX FAQ"
-  },
+      translations: {
+        "de-DE": "BibTeX FAQ",
+        "cs-CZ": "BibTeX FAQ",
+        "es-ES": "Preguntas frecuentes sobre BibTeX",
+        "pt-PT": "FAQ do BibTeX",
+        "fr-FR": "FAQ BibTeX",
+        "sv-SE": "BibTeX FAQ",
+        "tr-TR": "BibTeX SSS",
+        "zh-CN": "BibTeX 常见问题",
+        "da-DK": "BibTeX FAQ",
+        "it-IT": "FAQ su BibTeX",
+        "ja-JP": "BibTeX FAQ",
+        "ko-KR": "BibTeX FAQ",
+        "no-NO": "BibTeX FAQ",
+        "ru-RU": "BibTeX FAQ"
+      },
       autogenerate: {
         directory: 'faq'
       }
@@ -328,26 +318,23 @@ export default defineConfig({
         link: '/overleaf-citedrive/'
       }]
     }, {
-      
-        label: 'Links',
-        translations: {
-          "de-DE": "Links",
-          "cs-CZ": "Odkazy",
-          "es-ES": "Enlaces",
-          "pt-PT": "Links",
-          "fr-FR": "Liens",
-          "sv-SE": "Länkar",
-          "tr-TR": "Bağlantılar",
-          "zh-CN": "链接",
-          "da-DK": "Links",
-          "it-IT": "Collegamenti",
-          "ja-JP": "リンク",
-          "ko-KR": "링크",
-          "no-NO": "Lenker",
-          "ru-RU": "Ссылки"
-        },
-      
-      
+      label: 'Links',
+      translations: {
+        "de-DE": "Links",
+        "cs-CZ": "Odkazy",
+        "es-ES": "Enlaces",
+        "pt-PT": "Links",
+        "fr-FR": "Liens",
+        "sv-SE": "Länkar",
+        "tr-TR": "Bağlantılar",
+        "zh-CN": "链接",
+        "da-DK": "Links",
+        "it-IT": "Collegamenti",
+        "ja-JP": "リンク",
+        "ko-KR": "링크",
+        "no-NO": "Lenker",
+        "ru-RU": "Ссылки"
+      },
       items: [{
         label: '🔗 CiteDrive',
         link: 'https://www.citedrive.com/en/'
@@ -362,5 +349,5 @@ export default defineConfig({
         link: 'https://www.behaviorcloud.com/en/'
       }]
     }]
-  }), sitemap()]
+  }), sitemap(), react()]
 });
---
+++ src/content/docs/converters/website.tsx
@@ -0,0 +1,113 @@
+import React, { useState, type ChangeEvent, type FormEvent } from 'react';
+
+interface Metadata {
+  title?: string;
+  description?: string;
+  image?: string;
+  url?: string;
+  author?: string;
+  year?: number;
+}
+
+function getCurrentDate() {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = ("0" + (currentDate.getMonth() + 1)).slice(-2);
+    const day = ("0" + currentDate.getDate()).slice(-2);
+  
+    return `${year}-${month}-${day}`;
+  }
+
+const UrlInputComponent: React.FC = () => {
+  const [url, setUrl] = useState<string>('');
+  const [metadata, setMetadata] = useState<Metadata | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [useBibLaTeX, setUseBibLaTeX] = useState<boolean>(false);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setUrl(event.target.value);
+  };
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    setMetadata(null);
+    setError(null);
+  
+    try {
+      const response = await fetch(`https://api.microlink.io?url=${url}`);
+  
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+  
+      const data = await response.json();
+  
+      const metadata: Metadata = {
+        title: data.data.title,
+        description: data.data.description,
+        image: data.data.image?.url,
+        url: url,
+        author: data.data.author,
+        year: data.data.date,
+      };
+  
+      setMetadata(metadata);
+    } catch (error) {
+      setError('Failed to extract metadata');
+    }
+  };
+  
+
+  const formatBibTeX = (metadata: Metadata) => {
+    return `@misc{${metadata.title?.replace(/\s+/g, '_').toLowerCase() || 'citation'},
+  title        = {${metadata.title || 'No title'}},
+  author       = {${metadata.author || 'Unknown'}},
+  year         = ${metadata.year || 'Unknown'},
+  note         = {\\url{${metadata.url}} [Accessed: (${getCurrentDate()})]}
+}`;
+  };
+
+  const formatBibLaTeX = (metadata: Metadata) => {
+    return `@online{${metadata.title?.replace(/\s+/g, '_').toLowerCase() || 'citation'},
+  title        = {${metadata.title || 'No title'}},
+  author       = {${metadata.author || 'Unknown'}},
+  year         = ${metadata.year || 'Unknown'},
+  url          = {${metadata.url}},
+  urldate      = {${getCurrentDate()}}
+}`;
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Paste the URL of website you want to cite"
+          value={url}
+          onChange={handleChange}
+          style={{ width: '100%', padding: '8px', boxSizing: 'border-box' }}
+        />
+        <button type="submit">Cite website</button>
+      </form>
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            checked={useBibLaTeX}
+            onChange={() => setUseBibLaTeX(!useBibLaTeX)}
+          />
+          Use BibLaTeX format
+        </label>
+      </div>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      {metadata && (
+        <div>
+          <h3>{useBibLaTeX ? 'BibLaTeX Entry' : 'BibTeX Entry'}:</h3>
+          <pre>{useBibLaTeX ? formatBibLaTeX(metadata) : formatBibTeX(metadata)}</pre>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default UrlInputComponent;
