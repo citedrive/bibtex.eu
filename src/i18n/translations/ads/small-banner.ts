5f8cc2100a340cb5e83d04051e4b711a2cd3f253
garethbilaney <gareth.bilaney@hhu.de>
2025-04-12 10:33:27 +0200

    try a modal

--- src/i18n/translations/ads/small-banner.ts
+++ src/i18n/translations/ads/small-banner.ts
@@ -1,187 +1,187 @@
 export const SMALL_BANNER = {
 	en: {
 		SMALL_BANNER_H2:
-			'Need a simple solution for <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">managing</span> your <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">BibTeX</span> entries? Explore CiteDrive!',
+			'Need a simple solution for <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">managing</span> your <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">BibTeX</span> entries? Explore BibTeX Studio!',
 		SMALL_BANNER_LI_1: 'Web-based, modern reference management',
 		SMALL_BANNER_LI_2: 'Collaborate and share with fellow researchers',
-		SMALL_BANNER_LI_3: 'Integration with Overleaf',
-		SMALL_BANNER_LI_4: 'Comprehensive BibTeX/BibLaTeX support',
+		SMALL_BANNER_LI_3: 'Integration with <b>Overleaf</b>',
+		SMALL_BANNER_LI_4: 'Comprehensive <b>BibTeX/BibLaTeX</b> support',
 		SMALL_BANNER_LI_5: 'Save articles and websites directly from your browser',
 		SMALL_BANNER_LI_6: 'Search for new articles from a database of tens of millions of references',
-		SMALL_BANNER_BUTTON: 'Try out CiteDrive',
+		SMALL_BANNER_BUTTON: 'Try out BibTeX Studio',
 		SMALL_BANNER_VIDEO: '► Watch Demo'
 	},
 	de: {
 		SMALL_BANNER_H2:
-			'Brauchen Sie eine einfache Lösung zur <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">Verwaltung</span> Ihrer <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">BibTeX</span>-Einträge? Entdecken Sie CiteDrive!',
+			'Brauchen Sie eine einfache Lösung zur <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">Verwaltung</span> Ihrer <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">BibTeX</span>-Einträge? Entdecken Sie BibTeX Studio!',
 		SMALL_BANNER_LI_1: 'Webbasiertes, modernes Referenzmanagement',
 		SMALL_BANNER_LI_2: 'Kollaboration und Austausch mit Mitforschern',
-		SMALL_BANNER_LI_3: 'Integration mit Overleaf',
-		SMALL_BANNER_LI_4: 'Umfassende Unterstützung für BibTeX/BibLaTeX',
+		SMALL_BANNER_LI_3: 'Integration mit <b>Overleaf</b>',
+		SMALL_BANNER_LI_4: 'Umfassende Unterstützung für <b>BibTeX/BibLaTeX</b>',
 		SMALL_BANNER_LI_5: 'Artikel und Webseiten direkt aus Ihrem Browser speichern',
 		SMALL_BANNER_LI_6:
 			'Suche nach neuen Artikeln in einer Datenbank mit Zehnmillionen von Referenzen',
-		SMALL_BANNER_BUTTON: 'CiteDrive ausprobieren',
+		SMALL_BANNER_BUTTON: 'BibTeX Studio ausprobieren',
 		SMALL_BANNER_VIDEO: '► Demo ansehen'
 	},
 	cs: {
 		SMALL_BANNER_H2:
-			'Potřebujete jednoduché řešení pro <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">správu</span> vašich záznamů <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">BibTeX</span>? Objekvujte CiteDrive!',
+			'Potřebujete jednoduché řešení pro <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">správu</span> vašich záznamů <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">BibTeX</span>? Objekvujte BibTeX Studio!',
 		SMALL_BANNER_LI_1: 'Webová, moderní správa referencí',
 		SMALL_BANNER_LI_2: 'Spolupracujte a sdílejte s kolegy výzkumníky',
-		SMALL_BANNER_LI_3: 'Integrace s Overleaf',
-		SMALL_BANNER_LI_4: 'Komplexní podpora BibTeX/BibLaTeX',
+		SMALL_BANNER_LI_3: 'Integrace s <b>Overleaf</b>',
+		SMALL_BANNER_LI_4: 'Komplexní podpora <b>BibTeX/BibLaTeX</b>',
 		SMALL_BANNER_LI_5: 'Ukládejte články a webové stránky přímo z vašeho prohlížeče',
 		SMALL_BANNER_LI_6: 'Vyhledávejte nové články v databázi desítek milionů referencí',
-		SMALL_BANNER_BUTTON: 'Vyzkoušejte CiteDrive',
+		SMALL_BANNER_BUTTON: 'Vyzkoušejte BibTeX Studio',
 		SMALL_BANNER_VIDEO: '► Sledovat ukázku'
 	},
 	es: {
 		SMALL_BANNER_H2:
-			'¿Necesita una solución sencilla para <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">gestionar</span> sus entradas de <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">BibTeX</span>? ¡Explore CiteDrive!',
+			'¿Necesita una solución sencilla para <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">gestionar</span> sus entradas de <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">BibTeX</span>? ¡Explore BibTeX Studio!',
 		SMALL_BANNER_LI_1: 'Gestión de referencias moderna y basada en la web',
 		SMALL_BANNER_LI_2: 'Colaborar y compartir con colegas investigadores',
-		SMALL_BANNER_LI_3: 'Integración con Overleaf',
-		SMALL_BANNER_LI_4: 'Soporte integral para BibTeX/BibLaTeX',
+		SMALL_BANNER_LI_3: 'Integración con <b>Overleaf</b>',
+		SMALL_BANNER_LI_4: 'Soporte integral para <b>BibTeX/BibLaTeX</b>',
 		SMALL_BANNER_LI_5: 'Guardar artículos y sitios web directamente desde su navegador',
 		SMALL_BANNER_LI_6:
 			'Buscar nuevos artículos en una base de datos con decenas de millones de referencias',
-		SMALL_BANNER_BUTTON: 'Pruebe CiteDrive',
+		SMALL_BANNER_BUTTON: 'Pruebe BibTeX Studio',
 		SMALL_BANNER_VIDEO: '► Ver demostración'
 	},
 	pt: {
 		SMALL_BANNER_H2:
-			'Precisa de uma solução simples para <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">gerir</span> suas entradas <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">BibTeX</span>? Descubra o CiteDrive!',
+			'Precisa de uma solução simples para <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">gerir</span> suas entradas <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">BibTeX</span>? Descubra o BibTeX Studio!',
 		SMALL_BANNER_LI_1: 'Gestão de referências moderna e baseada na web',
 		SMALL_BANNER_LI_2: 'Colabore e compartilhe com colegas pesquisadores',
-		SMALL_BANNER_LI_3: 'Integração com o Overleaf',
-		SMALL_BANNER_LI_4: 'Suporte abrangente para BibTeX/BibLaTeX',
+		SMALL_BANNER_LI_3: 'Integração com o <b>Overleaf</b>',
+		SMALL_BANNER_LI_4: 'Suporte abrangente para <b>BibTeX/BibLaTeX</b>',
 		SMALL_BANNER_LI_5: 'Salve artigos e sites diretamente do seu navegador',
 		SMALL_BANNER_LI_6:
 			'Pesquise novos artigos em um banco de dados com dezenas de milhões de referências',
-		SMALL_BANNER_BUTTON: 'Experimente o CiteDrive',
+		SMALL_BANNER_BUTTON: 'Experimente o BibTeX Studio',
 		SMALL_BANNER_VIDEO: '► Ver demonstração'
 	},
 	fr: {
 		SMALL_BANNER_H2:
-			'Vous avez besoin d\'une solution simple pour <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">gérer</span> vos entrées <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">BibTeX</span>? Découvrez CiteDrive!',
+			'Vous avez besoin d\'une solution simple pour <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">gérer</span> vos entrées <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">BibTeX</span>? Découvrez BibTeX Studio!',
 		SMALL_BANNER_LI_1: 'Gestion de références moderne et basée sur le web',
 		SMALL_BANNER_LI_2: "Collaborez et partagez avec d'autres chercheurs",
-		SMALL_BANNER_LI_3: 'Intégration avec Overleaf',
-		SMALL_BANNER_LI_4: 'Support complet de BibTeX/BibLaTeX',
+		SMALL_BANNER_LI_3: 'Intégration avec <b>Overleaf</b>',
+		SMALL_BANNER_LI_4: 'Support complet de <b>BibTeX/BibLaTeX</b>',
 		SMALL_BANNER_LI_5:
 			'Enregistrez des articles et des sites web directement depuis votre navigateur',
 		SMALL_BANNER_LI_6:
 			'Recherchez de nouveaux articles dans une base de données de dizaines de millions de références',
-		SMALL_BANNER_BUTTON: 'Essayez CiteDrive',
+		SMALL_BANNER_BUTTON: 'Essayez BibTeX Studio',
 		SMALL_BANNER_VIDEO: '► Regarder la démo'
 	},
 	sv: {
 		SMALL_BANNER_H2:
-			'Behöver du en enkel lösning för att <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">hantera</span> dina <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">BibTeX</span>-poster? Upptäck CiteDrive!',
+			'Behöver du en enkel lösning för att <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">hantera</span> dina <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">BibTeX</span>-poster? Upptäck BibTeX Studio!',
 		SMALL_BANNER_LI_1: 'Webbaserad, modern referenshantering',
 		SMALL_BANNER_LI_2: 'Samarbeta och dela med forskarkollegor',
-		SMALL_BANNER_LI_3: 'Integration med Overleaf',
-		SMALL_BANNER_LI_4: 'Omfattande stöd för BibTeX/BibLaTeX',
+		SMALL_BANNER_LI_3: 'Integration med <b>Overleaf</b>',
+		SMALL_BANNER_LI_4: 'Omfattande stöd för <b>BibTeX/BibLaTeX</b>',
 		SMALL_BANNER_LI_5: 'Spara artiklar och webbplatser direkt från din webbläsare',
 		SMALL_BANNER_LI_6: 'Sök efter nya artiklar i en databas med tiotals miljoner referenser',
-		SMALL_BANNER_BUTTON: 'Prova CiteDrive',
+		SMALL_BANNER_BUTTON: 'Prova BibTeX Studio',
 		SMALL_BANNER_VIDEO: '► Titta på demo'
 	},
 	tr: {
 		SMALL_BANNER_H2:
-			'<span style="text-decoration: underline solid 5px rgb(0, 255, 107);">BibTeX</span> girişlerinizi <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">yönetmek</span> için basit bir çözüm mü arıyorsunuz? CiteDrive\'ı Keşfedin!',
+			'<span style="text-decoration: underline solid 5px rgb(0, 255, 107);">BibTeX</span> girişlerinizi <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">yönetmek</span> için basit bir çözüm mü arıyorsunuz? BibTeX Studio\'ı Keşfedin!',
 		SMALL_BANNER_LI_1: 'Web tabanlı, modern referans yönetimi',
 		SMALL_BANNER_LI_2: 'Araştırmacı meslektaşlarınızla işbirliği yapın ve paylaşın',
-		SMALL_BANNER_LI_3: 'Overleaf ile entegrasyon',
-		SMALL_BANNER_LI_4: 'Kapsamlı BibTeX/BibLaTeX desteği',
+		SMALL_BANNER_LI_3: '<b>Overleaf</b> ile entegrasyon',
+		SMALL_BANNER_LI_4: 'Kapsamlı <b>BibTeX/BibLaTeX</b> desteği',
 		SMALL_BANNER_LI_5: 'Makaleleri ve web sitelerini doğrudan tarayıcınızdan kaydedin',
 		SMALL_BANNER_LI_6: 'Milyonlarca referans içeren veritabanından yeni makaleler arayın',
-		SMALL_BANNER_BUTTON: "CiteDrive'ı Deneyin",
+		SMALL_BANNER_BUTTON: "BibTeX Studio'ı Deneyin",
 		SMALL_BANNER_VIDEO: '► Demo İzle'
 	},
 	'zh-cn': {
 		SMALL_BANNER_H2:
-			'需要一个简单的解决方案来<span style="text-decoration: underline solid 5px rgb(0, 255, 107);">管理</span>您的<span style="text-decoration: underline solid 5px rgb(0, 255, 107);">BibTeX</span>条目吗？探索CiteDrive！',
+			'需要一个简单的解决方案来<span style="text-decoration: underline solid 5px rgb(0, 255, 107);">管理</span>您的<span style="text-decoration: underline solid 5px rgb(0, 255, 107);">BibTeX</span>条目吗？探索BibTeX Studio！',
 		SMALL_BANNER_LI_1: '基于Web的现代参考文献管理',
 		SMALL_BANNER_LI_2: '与研究同行合作和分享',
-		SMALL_BANNER_LI_3: '与Overleaf集成',
-		SMALL_BANNER_LI_4: '全面的BibTeX/BibLaTeX支持',
+		SMALL_BANNER_LI_3: '与<b>Overleaf</b>集成',
+		SMALL_BANNER_LI_4: '全面的<b>BibTeX/BibLaTeX</b>支持',
 		SMALL_BANNER_LI_5: '直接从您的浏览器保存文章和网站',
 		SMALL_BANNER_LI_6: '从包含数千万参考文献的数据库中搜索新文章',
-		SMALL_BANNER_BUTTON: '尝试CiteDrive',
+		SMALL_BANNER_BUTTON: '尝试BibTeX Studio',
 		SMALL_BANNER_VIDEO: '► 观看演示'
 	},
 	da: {
 		SMALL_BANNER_H2:
-			'Har du brug for en enkel løsning til <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">at håndtere</span> dine <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">BibTeX</span>-poster? Udforsk CiteDrive!',
+			'Har du brug for en enkel løsning til <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">at håndtere</span> dine <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">BibTeX</span>-poster? Udforsk BibTeX Studio!',
 		SMALL_BANNER_LI_1: 'Webbaseret, moderne referencehåndtering',
 		SMALL_BANNER_LI_2: 'Samarbejd og del med forskerkolleger',
-		SMALL_BANNER_LI_3: 'Integration med Overleaf',
-		SMALL_BANNER_LI_4: 'Omfattende BibTeX/BibLaTeX-understøttelse',
+		SMALL_BANNER_LI_3: 'Integration med <b>Overleaf</b>',
+		SMALL_BANNER_LI_4: 'Omfattende <b>BibTeX/BibLaTeX</b>-understøttelse',
 		SMALL_BANNER_LI_5: 'Gem artikler og websteder direkte fra din browser',
 		SMALL_BANNER_LI_6: 'Søg efter nye artikler i en database med titusindvis af referencer',
-		SMALL_BANNER_BUTTON: 'Prøv CiteDrive',
+		SMALL_BANNER_BUTTON: 'Prøv BibTeX Studio',
 		SMALL_BANNER_VIDEO: '► Se demo'
 	},
 	it: {
 		SMALL_BANNER_H2:
-			'Hai bisogno di una soluzione semplice per <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">gestire</span> le tue voci <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">BibTeX</span>? Scopri CiteDrive!',
+			'Hai bisogno di una soluzione semplice per <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">gestire</span> le tue voci <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">BibTeX</span>? Scopri BibTeX Studio!',
 		SMALL_BANNER_LI_1: 'Gestione di riferimenti moderna e basata sul web',
 		SMALL_BANNER_LI_2: 'Collaborare e condividere con altri ricercatori',
-		SMALL_BANNER_LI_3: 'Integrazione con Overleaf',
-		SMALL_BANNER_LI_4: 'Supporto completo per BibTeX/BibLaTeX',
+		SMALL_BANNER_LI_3: 'Integrazione con <b>Overleaf</b>',
+		SMALL_BANNER_LI_4: 'Supporto completo per <b>BibTeX/BibLaTeX</b>',
 		SMALL_BANNER_LI_5: 'Salva articoli e siti web direttamente dal tuo browser',
 		SMALL_BANNER_LI_6: 'Cerca nuovi articoli in un database di decine di milioni di riferimenti',
-		SMALL_BANNER_BUTTON: 'Prova CiteDrive',
+		SMALL_BANNER_BUTTON: 'Prova BibTeX Studio',
 		SMALL_BANNER_VIDEO: '► Guarda la demo'
 	},
 	ja: {
 		SMALL_BANNER_H2:
-			'<span style="text-decoration: underline solid 5px rgb(0, 255, 107);">BibTeX</span>エントリーを<span style="text-decoration: underline solid 5px rgb(0, 255, 107);">管理</span>する簡単なソリューションが必要ですか？CiteDriveをご覧ください！',
+			'<span style="text-decoration: underline solid 5px rgb(0, 255, 107);">BibTeX</span>エントリーを<span style="text-decoration: underline solid 5px rgb(0, 255, 107);">管理</span>する簡単なソリューションが必要ですか？BibTeX Studioをご覧ください！',
 		SMALL_BANNER_LI_1: 'ウェブベースでモダンな参照管理',
 		SMALL_BANNER_LI_2: '研究者同士での共同作業と共有',
-		SMALL_BANNER_LI_3: 'Overleafとの統合',
-		SMALL_BANNER_LI_4: 'BibTeX/BibLaTeXの包括的なサポート',
+		SMALL_BANNER_LI_3: '<b>Overleaf</b>との統合',
+		SMALL_BANNER_LI_4: '<b>BibTeX/BibLaTeX</b>の包括的なサポート',
 		SMALL_BANNER_LI_5: 'ブラウザから直接記事とウェブサイトを保存',
 		SMALL_BANNER_LI_6: '数千万もの参照があるデータベースから新しい記事を検索',
-		SMALL_BANNER_BUTTON: 'CiteDriveを試す',
+		SMALL_BANNER_BUTTON: 'BibTeX Studioを試す',
 		SMALL_BANNER_VIDEO: '► デモを見る'
 	},
 	ko: {
 		SMALL_BANNER_H2:
-			'<span style="text-decoration: underline solid 5px rgb(0, 255, 107);">BibTeX</span> 항목을 <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">관리</span>할 간단한 솔루션이 필요하신가요? CiteDrive를 탐색해보세요!',
+			'<span style="text-decoration: underline solid 5px rgb(0, 255, 107);">BibTeX</span> 항목을 <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">관리</span>할 간단한 솔루션이 필요하신가요? BibTeX Studio를 탐색해보세요!',
 		SMALL_BANNER_LI_1: '웹 기반 현대적인 참고 문헌 관리',
 		SMALL_BANNER_LI_2: '동료 연구자들과 협업하고 공유하기',
-		SMALL_BANNER_LI_3: 'Overleaf와의 통합',
-		SMALL_BANNER_LI_4: 'BibTeX/BibLaTeX에 대한 포괄적인 지원',
+		SMALL_BANNER_LI_3: '<b>Overleaf</b>와의 통합',
+		SMALL_BANNER_LI_4: '<b>BibTeX/BibLaTeX</b>에 대한 포괄적인 지원',
 		SMALL_BANNER_LI_5: '브라우저에서 직접 문서와 웹사이트 저장',
 		SMALL_BANNER_LI_6: '수천만 개의 참조가 있는 데이터베이스에서 새로운 기사 검색',
-		SMALL_BANNER_BUTTON: 'CiteDrive 사용해보기',
+		SMALL_BANNER_BUTTON: 'BibTeX Studio 사용해보기',
 		SMALL_BANNER_VIDEO: '► 데모 보기'
 	},
 	no: {
 		SMALL_BANNER_H2:
-			'Trenger du en enkel løsning for <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">håndtering</span> av dine <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">BibTeX</span>-oppføringer? Utforsk CiteDrive!',
+			'Trenger du en enkel løsning for <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">håndtering</span> av dine <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">BibTeX</span>-oppføringer? Utforsk BibTeX Studio!',
 		SMALL_BANNER_LI_1: 'Nettbasert, moderne referansehåndtering',
 		SMALL_BANNER_LI_2: 'Samarbeid og del med forskerkolleger',
-		SMALL_BANNER_LI_3: 'Integrasjon med Overleaf',
-		SMALL_BANNER_LI_4: 'Omfattende støtte for BibTeX/BibLaTeX',
+		SMALL_BANNER_LI_3: 'Integrasjon med <b>Overleaf</b>',
+		SMALL_BANNER_LI_4: 'Omfattende støtte for <b>BibTeX/BibLaTeX</b>',
 		SMALL_BANNER_LI_5: 'Lagre artikler og nettsteder direkte fra nettleseren din',
 		SMALL_BANNER_LI_6: 'Søk etter nye artikler i en database med titalls millioner av referanser',
-		SMALL_BANNER_BUTTON: 'Prøv CiteDrive',
+		SMALL_BANNER_BUTTON: 'Prøv BibTeX Studio',
 		SMALL_BANNER_VIDEO: '► Se demo'
 	},
 	ru: {
 		SMALL_BANNER_H2:
-			'Нужно простое решение для <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">управления</span> вашими записями <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">BibTeX</span>? Ознакомьтесь с CiteDrive!',
+			'Нужно простое решение для <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">управления</span> вашими записями <span style="text-decoration: underline solid 5px rgb(0, 255, 107);">BibTeX</span>? Ознакомьтесь с BibTeX Studio!',
 		SMALL_BANNER_LI_1: 'Веб-основанное, современное управление библиографическими ссылками',
 		SMALL_BANNER_LI_2: 'Сотрудничество и обмен с коллегами-исследователями',
-		SMALL_BANNER_LI_3: 'Интеграция с Overleaf',
-		SMALL_BANNER_LI_4: 'Полноценная поддержка BibTeX/BibLaTeX',
+		SMALL_BANNER_LI_3: 'Интеграция с <b>Overleaf</b>',
+		SMALL_BANNER_LI_4: 'Полноценная поддержка <b>BibTeX/BibLaTeX</b>',
 		SMALL_BANNER_LI_5: 'Сохраняйте статьи и веб-сайты прямо из вашего браузера',
 		SMALL_BANNER_LI_6: 'Поиск новых статей в базе данных из десятков миллионов ссылок',
-		SMALL_BANNER_BUTTON: 'Попробовать CiteDrive',
+		SMALL_BANNER_BUTTON: 'Попробовать BibTeX Studio',
 		SMALL_BANNER_VIDEO: '► Смотреть демонстрацию'
 	}
 }
